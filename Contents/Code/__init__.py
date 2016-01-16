a2c730f39294ba06a7837cfd678038b7c4b3a4e8
Solvek <sa@mobiwolf.com>
2016-01-16 14:34:32 +0200

    Rewrote station listing

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,18 +1,12 @@
-import json
-
 ROOT_MENU = 'http://opml.radiotime.com/Browse.ashx?formats=mp3,aac'
 
 ART    = 'art-default.jpg'
 ICON   = 'icon-default.png'
 ICON_MORE   = 'arrow-next-2-icon.png'
 
-MY_STATIONS_PREFIX = 'mystations:'
-
-RE_USER_ID = Regex('"HeroRibbon"\:\{"guideId"\:"(?P<userid>[^"]+)"')
-
-USER_PAGE = "http://tunein.com/user/%s/"
-STANTIONS_PAGE = "http://tunein.com/profile/follows/?identifier=%s/follows/stations&type=&offset=%s"
-STATION_URL = 'http://opml.radiotime.com/Tune.ashx?id=%s&formats=mp3,aac'
+STATION_URL = 'http://tunein.com/radio/-%s/'
+USER_URL = 'http://opml.radiotime.com/Browse.ashx?c=presets&partnerId=RadioTime&username=%s'
+MY_STATIONS = 'tunein://mystations'
 
 ####################################################################################################
 def Start():
@@ -31,126 +25,68 @@ def ValidatePrefs():
 ####################################################################################################
 @handler('/music/tunein', 'TuneIn')
 @route('/music/tunein/menu')
-def Menu(url=ROOT_MENU, title='', outline_text=''):
+def Menu(url=ROOT_MENU, title='', xml = None):
 
     oc = ObjectContainer(title2=title)
 
     if url == ROOT_MENU:
-        oc.add(MyStationsDirectory(L('My Stations'), 0))
+        my_stations = L('My Stations')
+        oc.add(DirectoryObject(
+            key = Callback(Menu, url=MY_STATIONS, title=my_stations),
+            title = my_stations
+        ))
         oc.add(PrefsObject(
                 title   = L('Preferences...'),
             )
         )
 
-    if (url.startswith(MY_STATIONS_PREFIX)):
-        MakeMyStationsList(oc, url)
+    if url == MY_STATIONS:
+        username = Prefs['username']
+
+        if not username:
+            oc.message = L('Please specify user name in the TuneIn preferences')
+            return oc
+
+        url = USER_URL % username
+
+    if xml:
+        subitems = XML.ElementFromString(xml)
     else:
-        MakeDefaultStationsList(oc, url, outline_text)
+        subitems = XML.ElementFromURL(url).xpath('//body')[0]
+
+    # Log.Debug('Subitems: '+str(subitems))
+    Log.Debug('Subitems count: '+str(len(subitems)))
+
+    for item in subitems:
+        typ = item.get('type')
+        local_url = item.get('URL')
+        text = item.get('text')
+        image = item.get('image')
+        key = item.get('key')
+        subtext = item.get('subtext')
+
+        if key in ['unavailable', 'related']:
+            continue
+
+        if typ == 'audio':
+            oc.add(TrackObject(
+                url = local_url,
+                title = text,
+                summary = subtext,
+                thumb = Resource.ContentsOfURLWithFallback(image)
+            ))
+        elif typ == 'link':
+            oc.add(DirectoryObject(
+                key = Callback(Menu, url=local_url, title=text),
+                title = text,
+                thumb = Resource.ContentsOfURLWithFallback('')
+            ))
+        else:
+            # Log.Debug('Current item: '+str(item))
+            oc.add(DirectoryObject(
+                key = Callback(Menu, title=text, xml = XML.StringFromElement(item)),
+                title = text,
+                thumb = Resource.ContentsOfURLWithFallback('')
+            ))
 
     return oc
-
-####################################################################################################
-def MyStationsDirectory(title, offset):
-
-    return DirectoryObject(
-                key = Callback(Menu, url=MY_STATIONS_PREFIX+str(offset), title=title, outline_text=title),
-                title = title,
-                thumb = R(ICON_MORE)
-            )
-
-####################################################################################################
-def MakeMyStationsList(oc, url):
-
-    if not Prefs['username']:
-        oc.message = L('Please specify user name in the TuneIn preferences')
-        return
-
-    userid = GetUserId()
-    offset = int(url.split(':')[1])
-
-    Log.Debug("Requesting for stations. userid=%s, offset: %s" % (userid, offset))
-
-    text = HTTP.Request(
-                        STANTIONS_PAGE % (userid, offset),
-                        headers={'x-requested-with': 'XMLHttpRequest'}
-                        ).content
-
-    js  = json.loads(text)
-
-    counter = 0
-
-    for station in js['payload']['guideItemGroups']:
-        station_id = station['GuideId']
-
-        track = TrackObject(
-            url = STATION_URL % station_id,
-            title = station['Title'],
-            thumb = Resource.ContentsOfURLWithFallback(station['Image']),
-            summary = station['Subtitle']
-        )
-        oc.add(track)
-
-        counter += 1
-
-    oc.add(MyStationsDirectory('More', offset+counter))
-
-####################################################################################################
-def MakeDefaultStationsList(oc, url, outline_text):
-
-    root = XML.ElementFromURL(url).xpath('//body')[0]
-
-    if len(root.xpath('./outline[@URL and not(@type="audio")]')) > 0:
-        for item in root.xpath('./outline[@URL]'):
-
-            oc.add(DirectoryObject(
-                key = Callback(Menu, url=item.get('URL'), title=item.get('text')),
-                title = item.get('text'),
-                thumb = Resource.ContentsOfURLWithFallback('')
-            ))
-
-    if len(root.xpath('./outline[@text and not(@URL) and not(@key="related")]')) > 0 and outline_text == '':
-        for item in root.xpath('./outline[@text and not(@URL) and not(@key="related")]'):
-
-            if item.get('text') == 'This program is not available':
-                continue
-
-            oc.add(DirectoryObject(
-                key = Callback(Menu, url=url, title=item.get('text'), outline_text=item.get('text')),
-                title = item.get('text'),
-                thumb = Resource.ContentsOfURLWithFallback('')
-            ))
-
-    if outline_text != '':
-        for item in root.xpath('./outline[@text="%s"]/outline' % outline_text):
-
-            if item.get('type') == 'link':
-
-                oc.add(DirectoryObject(
-                    key = Callback(Menu, url=item.get('URL'), title=item.get('text')),
-                    title = item.get('text'),
-                    thumb = Resource.ContentsOfURLWithFallback(item.get('image'))
-                ))
-
-            elif item.get('type') == 'audio':
-                Log.Debug("Station XML: "+XML.StringFromElement(root))
-
-                oc.add(TrackObject(
-                    url = item.get('URL'),
-                    title = item.get('text'),
-                    thumb = Resource.ContentsOfURLWithFallback(item.get('image'))
-                ))
-
-####################################################################################################
-def GetUserId():
-
-    if Dict['username'] == Prefs['username']:
-        return Dict['userid']
-
-    content = HTTP.Request(USER_PAGE % Prefs['username']).content
-
-    userid = RE_USER_ID.search(content).group('userid')
-
-    Dict['username'] = Prefs['username']
-    Dict['userid'] = userid
-
-    return userid
